c365470f6177c93acccb6fa63e151865aa620c64
Dev Karan Singh <dev1604karan@gmail.com>
2025-08-01 16:31:15 +0530

    day 1 - feat: Initialize devtinder project with package.json, requirements gathering, and basic Express server setup
    - Added package.json for project dependencies and scripts
    - Created requirements gathering document outlining features and tech stack
    - Implemented basic Express server with routes for test, profile, and dashboard

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+//instance of an express js application
+const app = express();
+
+//request handler function
+app.use("/test", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.use("/profile", (req, res) => {
+  res.send("Hello from the profile!");
+});
+
+app.use("/dashboard", (req, res) => {
+  res.send("Hello from the dasboard!");
+});
+
+//port - 30000
+app.listen(3000, () => {
+  console.log(
+    "Server is listening on port 3000......"
+  );
+});
