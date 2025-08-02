837f2536dae4c25470c8eea16c4d13dddc265150
Dev Karan Singh <dev1604karan@gmail.com>
2025-08-02 17:17:38 +0530

    day 2 - feat: Update app.js with new route handlers and enhance comment clarity; add VSCode settings for comment color customization

--- src/app.js
+++ src/app.js
@@ -3,20 +3,58 @@ const express = require("express");
 //instance of an express js application
 const app = express();
 
-app.set;
+//Order of how routes are written matter
 
 //request handler function
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
+// app.use("/", (req, res) => {
+//   res.send("Hello from the server!");
+// }); //if
+// //  this route is alwaays written first, the request will be sent to this route only. It wont check the other routes like '/hello' etc.
+
+// app.get("/user", (req, res) => {
+//   res.send({
+//     firstName: "Dev Karan",
+//     lastName: "Singh",
+//   });
+// });
+//request handler function
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the test!");
+// });
+
+// app.use("/profile", (req, res) => {
+//   res.send("Hello from the profile!");
+// });
+
+// app.use("/dashboard", (req, res) => {
+//   res.send("Hello from the dasboard!");
+// });
+
+//Advanced concept
+app.get("/abc", (req, res) => {
+  res.send({
+    firstName: "Gary",
+    lastName: "Singh",
+  });
 });
 
-app.use("/profile", (req, res) => {
-  res.send("Hello from the profile!");
-});
+app.use(
+  "/user",
+  (req, res) => {
+    //route handler 1
+    console.log("Route handled for 1");
+    res.send("API route handled for 1");
+  },
+  (req, res) => {
+    //route handler 2
+    console.log("Route handled for 2");
+    res.send("API route handled for 2");
+  }
+);
 
-app.use("/dashboard", (req, res) => {
-  res.send("Hello from the dasboard!");
-});
+//if we add '?' in front of a char, that char becomes optional in route
+//if we put '+' in front of a char, that char can be types multiple times in the string of the route
+//REGULAR Expressions work in the routes
 
 //port - 30000
 app.listen(3000, () => {
